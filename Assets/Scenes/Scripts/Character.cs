65677f066f3955357760f6ccc21c146acc6366f5
AtumRamirez <a59660@alunos.ipb.pt>
2025-02-26 16:26:28 +0000

    Make your game frame rate independent

--- Assets/Scenes/Scripts/Character.cs
+++ Assets/Scenes/Scripts/Character.cs
@@ -3,47 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Character : MonoBehaviour
+
+public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-
-            horizontal = -1.0f;
-
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-
-            horizontal = 1.0f;
-
-        }
-
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-
-            vertical = 1.0f;
-
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-
-            vertical = -1.0f;
-
-        }
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
-
     }
+
+
 }
